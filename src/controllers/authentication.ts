968f74371649314d306432adfc399eda33946870
Ernest Manukyan <erik.manukyan2010@yandex.ru>
2024-03-09 17:01:06 +0400

    chore: test

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -90,7 +90,7 @@ export const login = async (req: Request, res: Response) => {
 			httpOnly: true,
 			// maxAge: config.COOKIE_TOKEN_LIFETIME,
 			sameSite: config.isProduction ? "none" : "lax",
-			// secure: config.isProduction,
+			secure: config.isProduction,
 			path: "/",
 		});
 
