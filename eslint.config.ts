b9d3f11527f520dfa36966fc9005704b3b299d2f
Ernest Manukyan <erik.manukyan2010@yandex.ru>
2024-04-24 14:38:05 +0400

    feat: adding eslint and eslint config

---
+++ eslint.config.ts
@@ -0,0 +1,11 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  formatters: true,
+}, {
+  files: ['**/*.ts'],
+  rules: {
+    'node/prefer-global/process': 'off',
+    'no-console': 'off',
+  },
+})
