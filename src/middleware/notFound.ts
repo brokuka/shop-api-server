6883c287b99b4163123f6d7443bd76e7a74ed2ee
Ernest Manukyan <erik.manukyan2010@yandex.ru>
2024-04-28 01:11:14 +0400

    chore: update welcome message

--- src/middleware/notFound.ts
+++ src/middleware/notFound.ts
@@ -3,7 +3,7 @@ import { customResponse, errorResponse } from '../utils/common.js'
 
 export function notFound(req: Request, res: Response, next: NextFunction) {
   if (req.path === '/')
-    return customResponse(res, 'WELCOME_MESSAGE')
+    return customResponse(res, { message: 'WELCOME_MESSAGE' })
 
   errorResponse(res, 'NOT_FOUND')
   next()
