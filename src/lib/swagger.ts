ded7ca8588c7642a42bbcebccd0ecd6dab7b93da
Ernest Manukyan <erik.manukyan2010@yandex.ru>
2024-04-28 01:52:53 +0400

    chore: update swagger options

--- src/lib/swagger.ts
+++ src/lib/swagger.ts
@@ -1,37 +1,8 @@
 import type { Options } from 'swagger-jsdoc'
-import { AUTH_SCHEMAS, CART_SCHEMAS, ORDER_SCHEMAS, PRODUCT_SCHEMAS, USER_SCHEMAS, UTIL_SCHEMAS } from '../schemas/swagger.js'
 import packageDoc from '../../package.json' assert { type: 'json' }
 
-// export const swaggerOptions: Options = {
-//   definition: {
-//     openapi: '3.0.3',
-//     info: {
-//       title: 'Shop Api',
-//       description: 'Документация по API',
-//       version: packageDoc.version,
-//     },
-//     components: {
-//       securitySchemes: {
-//         bearerAuth: {
-//           type: 'http',
-//           scheme: 'bearer',
-//           bearerFormat: 'JWT',
-//         },
-//       },
-//       schemas: {
-//         ...AUTH_SCHEMAS,
-//         ...PRODUCT_SCHEMAS,
-//         ...CART_SCHEMAS,
-//         ...ORDER_SCHEMAS,
-//         ...USER_SCHEMAS,
-//         ...UTIL_SCHEMAS,
-//       },
-//     },
-//   },
-//   apis: ['./src/router/*.*'],
-// }
 export function swaggerOptions({ ...schemas }: any) {
-  return {
+  return <Options>{
     definition: {
       openapi: '3.0.3',
       info: {
